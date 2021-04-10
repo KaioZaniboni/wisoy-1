cc728ab9afc7c361830539e98b0d84745c934404
Matheus Vieck <matheus.mvdd@gmail.com>
2021-04-10 17:03:50 -0300

    Atualização 10/04

--- sql/wisoy.sql
+++ sql/wisoy.sql
@@ -4,17 +4,18 @@
 CREATE DATABASE wisoy;
 USE wisoy;
 
+drop database wisoy;
+
 -- -----------------------------------------------------
 -- A tabela LEADS sera responsável por armazenar as informações referentes as pessoas que acessaram o site e se cadastraram nesse campo, são os interessados.
 -- -----------------------------------------------------
 CREATE TABLE LEADS (
   ID_LEAD INT NOT NULL AUTO_INCREMENT,
-  NOME VARCHAR(60),
-  SEXO CHAR(1),
+  NOME_FANTASIA VARCHAR(60),
   EMAIL VARCHAR(60),
   TELEFONE VARCHAR(11),
-  CPF_CNPJ VARCHAR(23),
-  PRIMARY KEY (ID_LEAD))auto_increment = 1;
+  CNPJ VARCHAR(23),
+  PRIMARY KEY (ID_LEAD))auto_increment = 100;
 
 DESC LEADS;
 -- -----------------------------------------------------
@@ -22,27 +23,29 @@ DESC LEADS;
 -- -----------------------------------------------------
 CREATE TABLE CLIENTES (
   ID_CLIENTE INT NOT NULL AUTO_INCREMENT,
-  NOME VARCHAR(60),
-  SEXO CHAR(1),
-  DATA_NASCIMENTO DATE,
-  EMAIL VARCHAR(60),
-  USUARIO VARCHAR(20),
-  SENHA VARCHAR(20),
+  NOME_FANTASIA VARCHAR(60),
   TELEFONE VARCHAR(11),
-  RG CHAR(9),
-  CPF_CPNJ VARCHAR(23),
+  EMAIL VARCHAR (50),
+  CPNJ VARCHAR(23),
   RAZAO_SOCIAL VARCHAR(50),
-  ENDERECO_CLIENTE VARCHAR(100),
-  DADOS_BANCARIOS CHAR(27),
   DATA_CONTRATO DATE,
   PRIMARY KEY (ID_CLIENTE),
   FK_LEAD INT,
-  FOREIGN KEY (FK_LEAD) REFERENCES LEADS (ID_LEAD),
-  FK_FUNCIONARIOS INT,
-  FOREIGN KEY (FK_FUNCIONARIOS) REFERENCES FUNCIONARIOS(ID_FUNCIONARIO)
+  FOREIGN KEY (FK_LEAD) REFERENCES LEADS (ID_LEAD)
 )auto_increment = 15000;
 
 DESC CLIENTES;
+
+--------------------------------------------------------
+-- A tabela USUARIOS_CLIENTES é responsável pelos login no sistema para demais verificações
+--------------------------------------------------------
+
+CREATE TABLE USUARIOS_CLIENTES(
+ID_USUARIOS_CLIENTES INT PRIMARY KEY AUTO_INCREMENT,
+LOGIN VARCHAR (35),
+SENHA VARCHAR (20),
+FK_CLIENTES INT,
+FOREIGN KEY (FK_CLIENTES) REFERENCES CLIENTES (ID_CLIENTE));
 -- -----------------------------------------------------
 -- A tabela FAZENDAS será responsável por armazenar os dados referentes às fazendas dos clientes.
 -- -----------------------------------------------------
@@ -53,7 +56,6 @@ CREATE TABLE FAZENDAS(
   NUMERO_SENSORES INT,
   ENDERECO_FAZENDA VARCHAR(100),
   PRIMARY KEY (ID_FAZENDA),
-  CONSTRAINT FK_FAZENDAS_CLIENTES
   FOREIGN KEY (FK_CLIENTE)
   REFERENCES CLIENTES (ID_CLIENTE))auto_increment = 1;
 
@@ -65,8 +67,8 @@ CREATE TABLE SENSORES (
   ID_SENSOR INT NOT NULL AUTO_INCREMENT,
   FK_FAZENDA INT NOT NULL,
   DATA_INSTALACAO DATE,
+  SENSOR_XY VARCHAR (45),
   PRIMARY KEY (ID_SENSOR),
-  CONSTRAINT FK_SENSORES_FAZENDAS
   FOREIGN KEY (FK_FAZENDA)
   REFERENCES FAZENDAS (ID_FAZENDA))auto_increment = 1;
 
@@ -74,21 +76,21 @@ DESC SENSORES;
 -- -----------------------------------------------------
 -- A tabela FUNCIONARIOS contera dados de todo quadro de funcionários WiSoy.
 -- -----------------------------------------------------
-CREATE TABLE FUNCIONARIOS (
-  ID_FUNCIONARIO INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE COLABORADORES (
+  ID_COLABORADORES INT NOT NULL AUTO_INCREMENT,
   NOME VARCHAR(60),
   SEXO CHAR(1),
   DATA_NASCIMENTO DATE,
   EMAIL VARCHAR(60),
-  USUARIO VARCHAR(20),
+  LOGIN VARCHAR(20),
   SENHA VARCHAR(20),
   TELEFONE VARCHAR(11),
   RG CHAR(9),
   CPF CHAR(11),
   ENDERECO VARCHAR(100) ,
   CARTEIRA_TRABALHO CHAR(8),
-  DATA_CONTRATO DATE,
-  PRIMARY KEY (ID_FUNCIONARIO))auto_increment = 10000;
+  DATA_ADMISSAO DATE,
+  PRIMARY KEY (ID_COLABORADORES))auto_increment = 1000;
 
 DESC FUNCIONARIOS;
 -- -----------------------------------------------------
@@ -97,17 +99,14 @@ DESC FUNCIONARIOS;
 CREATE TABLE OCORRENCIAS (
   ID_OCORRENCIA INT NOT NULL AUTO_INCREMENT,
   FK_CLIENTE INT NOT NULL,
-  FK_FUNCIONARIO INT NOT NULL,
+  FK_COLABORADORES INT NOT NULL,
   RELATO VARCHAR(280),
   DATA_OCORRENCIA DATE,
-  DATA_RELATO DATE,
   PRIMARY KEY (ID_OCORRENCIA),
-  CONSTRAINT FK_OCORRENCIAS_CLIENTES
   FOREIGN KEY (FK_CLIENTE)
   REFERENCES CLIENTES (ID_CLIENTE),
-  CONSTRAINT FK_OCORRENCIAS_FUNCIONARIOS
-  FOREIGN KEY (FK_FUNCIONARIO)
-  REFERENCES FUNCIONARIOS (ID_FUNCIONARIO))auto_increment = 1;
+  FOREIGN KEY (FK_COLABORADORES)
+  REFERENCES COLABORADORES (ID_COLABORADORES))auto_increment = 1;
 
 DESC OCORRENCIAS;
 -- -----------------------------------------------------
@@ -116,11 +115,8 @@ DESC OCORRENCIAS;
 CREATE TABLE DADOS (
   ID_DADOS INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   FK_SENSOR INT NOT NULL,
-  SENSOR_XY CHAR(17),
-  ESTADO_SENSOR VARCHAR(9),
   LEITURA_UMIDADE CHAR(6),
   LEITURA_DATA_HORA CHAR(16),
-  CONSTRAINT FK_DADOS_SENSORES
   FOREIGN KEY (FK_SENSOR)
   REFERENCES SENSORES (ID_SENSOR))auto_increment = 1;
   
@@ -129,76 +125,109 @@ CREATE TABLE DADOS (
 -- Inserts para testes
 -- -----------------------------------------------------
 
-select * from leads;
-desc leads;
-
-INSERT INTO LEADS VALUES (NULL, 'Felipe Silva', 'M', 'felipe_silva@gmail.com', '11987654321', '45433595534'),
-(NULL, 'Gabriel Pereira', 'M', 'gabriel_pereira@hotmail.com', '11912345678', '55345678000145'),
-(NULL, 'Vanessa Almeida', 'F', 'vanessa.almeida@outlook.com.br', '11987774571', '98765432105'),
-(NULL, 'Camila Nascimento', 'F', 'camila.nasc@gmail.com', '11995154321', '45433984534'),
-(NULL, 'Andre Andrade', 'M', 'andre_andrade@gmail.com', '11955774205', '12345678000164');
+INSERT INTO LEADS VALUES (NULL, 'Project Plant', 'contato@projectplant.com', '11987654321', '45433595000134'),
+(NULL, 'Soja Mais', 'soja_mais@hotmail.com', '11912345678', '55345678000145'),
+(NULL, 'Edinaldo Soja', 'edinal.soja@outlook.com.br', '11987774571', '98765432100015'),
+(NULL, 'Agro Soja', 'contato@agrosoja.com.br', '11995154321', '45433984534143'),
+(NULL, 'Agro Farm', 'agro_farm@gmail.com.br', '11955774205', '12345678000164');
 
 
 SELECT * FROM LEADS;
 
 DESC CLIENTES;
 
-INSERT INTO CLIENTES VALUES 
-(NULL, 'Felipe Silva', 'M', '1980-08-19', 'felipe_silva@gmail.com', 'felipe_soja1', 
-'A34zt7@3', '11987654321', '590053339', '45433595534', 'Project Plant', 'Rua Engenheiro Marcelo Torres', '7582-000254', '2021-03-08', 1, 10000),
-(NULL, 'Leandro Santos', 'M', '1977-02-23', 'leandro.santos@gmail.com', 'leandro_sssoja', 
-'Z58tv9#1', '11987645678', '597755439', '43404695987', 'Soja Company', 'Rua Capitão Felipe Smith', '4872-025271', '2021-04-07', null, 10001),
-(NULL, 'Vanessa Almeida', 'F', '1990-01-15', 'vanessa.almeida@outlook.com.br', 'vanessa_topo', 'T@@z7f41', '11987774571', 
-'435994729', '98765432105', 'Topo Soja Plant', 'Rua Professora Amelia Nascimento', '3540-000759', '2021-02-13', 3, 10002),
-(NULL, 'Andre Andrade', 'M', '1982-07-26', 'andre_andrade@gmail.com', 'andre07_world', 'W07#45ab#26', '11955774205', 
-'756348167', '12345678000164', 'World Soja', 'Rua Encomendador Cornelius', '4000-305789', '2021-04-03', 5, 10002),
-(NULL, 'Marco Oliveira', 'M', '1979-06-06', 'marco.oliveira@hotmail.com.br', 'marco_agro', 'Ma@552f37#', '11950427731', 
-'675424719', '54816789000135', 'Soja Mais', 'Avenida Gomes Mendes', '7570-124994', '2021-01-03', null, 10000);
+SELECT * FROM CLIENTES;
 
+INSERT INTO CLIENTES VALUES 
+(NULL, 'Project Plant', '11987654321', 'contato@projectplant.com', '45433595000134', 'João Plantação Agro', '2021-03-08',  100),
+(NULL, 'Unilever', '11987880572', 'contato@projectplant.com', '61068276000104', 'Unilever LTDA', '2021-01-15',  null),
+(NULL, 'Agro Farm', '11955774205', 'agro_farm@gmail.com.br', '12345678000164', 'Agro Farms Comercio de Graos Eireli', '2021-04-08',  104),
+(NULL, 'Soja Hoje', '11966196606', 'soja_hoje@hotmail.com.br', '45455595000144', 'Soja Hoje LTDA', '2021-02-18',  null),
+(NULL, 'Coca Cola', '11954320706', 'contato@cocacola.com.br', '45997418000153', 'COCA COLA INDUSTRIAS LTDA', '2020-12-23',  null);
 
 SELECT * FROM CLIENTES;
 DESC CLIENTES;
 
-INSERT INTO FAZENDAS VALUES(NULL, 1, 1250, 42, 'endereço fazenda01');
-INSERT INTO FAZENDAS VALUES(NULL, 1, 625, 21, 'endereço fazenda02');
-INSERT INTO FAZENDAS VALUES(NULL, 2, 30, 1, 'endereço fazenda03');
-INSERT INTO FAZENDAS VALUES(NULL, 2, 60, 2, 'endereço fazenda04');
-SELECT * FROM FAZENDAS;
+SELECT * FROM USUARIOS_CLIENTES;
 
-INSERT INTO SENSORES VALUES (NULL, 1, '2021-03-11');
-INSERT INTO SENSORES VALUES (NULL, 2, '2020-03-11');
+INSERT INTO USUARIOS_CLIENTES VALUES 
+(null, 'user1_plant', 'P@3507A', 15000), (null, 'user2_plant', 'P@5700A', 15000), (null, 'user3_plant', 'P#5503A', 15000),
+(null, 'user1_lever', 'U@4433S', 15001), (null, 'user2_lever', 'U#3211S', 15001), (null, 'user3_lever', 'U@6671S', 15001),
+(null, 'user1_farm', 'F@3257A', 15002), (null, 'user2_farm', 'F#7707A', 15002),
+(null, 'user1_shoje', 'S@4302H', 15003), (null, 'user2_shoje', 'S#2002H', 15003), (null, 'user2_shoje', 'S@5552H', 15003),
+(null, 'user1_cola', 'C@5504C', 15004), (null, 'user2_cola', 'C!2505C', 15004);
+
+SELECT * FROM USUARIOS_CLIENTES;
+
+SELECT * FROM CLIENTES;
+
+INSERT INTO FAZENDAS VALUES
+(NULL, 15000, 1000, 34, 'Rua Padre André'), (NULL, 15000, 90, 3, 'Rua Andrade Lopes'),
+(NULL, 15001, 1500, 50 , 'Rua Fernando Moreira'),
+(NULL, 15002, 400, 14, 'Rua Cornelius Frederick'), (NULL, 15002, 700, 24, 'Avenida Sapopemba'),
+(NULL, 15003, 630, 21, 'Rua Engenheiro Alberto Nilman'),
+(NULL, 15004, 10000, 334, 'Rua Coronel Armando');
+
+SELECT * FROM FAZENDAS;
+SELECT * FROM CLIENTES;
+
+INSERT INTO SENSORES VALUES 
+(NULL, 1, '2021-03-11', '86.1638, -80.4437'), (NULL, 2, '2021-03-16', '-57.0430, -157.2459'),
+(NULL, 3, '2021-01-25', '42.1108, 7.6416'), (NULL, 4, '2021-03-11', '70.2003, -165.4415'),
+(NULL, 5, '2021-03-11', '-75.3722, -176.6609'), (NULL, 6, '2021-02-28', '-34.8391, 157.6925'),
+(NULL, 7, '2021-01-02', '22.5029, 136.9261');
+
+
 SELECT * FROM SENSORES;
 
 INSERT INTO DADOS VALUES (1, 'X: 77.77 Y: 33.33', 'LIGADO', '13.47%', '2022-28-03 14:55');
-INSERT INTO DADOS VALUES (1, 'X: 77.77 Y: 33.33', 'LIGADO', '16.35%', '2022-28-03 17:23');
-INSERT INTO DADOS VALUES (2, 'X: 32.03 Y: 50.90', 'LIGADO', '13.47%', '2022-28-03 17:55');
-INSERT INTO DADOS VALUES (2, 'X: 32.03 Y: 50.90', 'LIGADO', '16.35%', '2022-28-03 19:03');
+
 SELECT * FROM DADOS;
 
-INSERT INTO FUNCIONARIOS VALUES(NULL, 'Jonas Florencio', 'M', '2002-03-30', 'jonas_florencio@wisoy.com.br', 'jonas_wisoy', 
+INSERT INTO COLABORADORES VALUES(NULL, 'Jonas Florencio', 'M', '2002-03-30', 'jonas_florencio@wisoy.com.br', 'jonas_wisoy', 
 'Jo54@p12', '11955250037', '503032217', '33299400574', 'Rua Professor Otavio Fernandes', '88833397', '2021-02-01'),
 (NULL, 'Amanda Fruteiro', 'F', '2002-01-07', 'amanda_fruteiro@wisoy.com.br', 'amanda_wisoy', 
-'Am01#dsC', '11944064307', '243555719', '46955749916', 'Rua Enfermeira Luiza Alegreti', '88833397', '2021-02-01'),
+'Am01#dsC', '11944064307', '243555719', '46955749916', 'Rua Enfermeira Luiza Alegreti', '77433397', '2021-02-01'),
 (NULL, 'Matheus Vieck', 'M', '1998-02-13', 'matheus_vieck@wisoy.com.br', 'matheus_wisoy', 
-'Ma37#f13', '11955250037', '503032217', '33299400574', 'Rua Soldado Peixoto', '88833397', '2021-02-01');
+'Ma37#f13', '11955250037', '530075995', '46933797734', 'Rua Soldado Peixoto', '74421388', '2021-02-01');
 
-SELECT * FROM FUNCIONARIOS;
-DESC FUNCIONARIOS;
+SELECT * FROM COLABORADORES;
 
+INSERT INTO OCORRENCIAS VALUES
+(NULL, 15000, 1000, 'Fazenda da Rua Andrade Lopes problema no sensor.', '2021-03-27'),
+(NULL, 15000, 1000, 'Fazenda da Rua Padre André precisa trocar sensor 86.1638, -80.4437', '2021-03-28'),
+(NULL, 15001, 1001, 'Fazenda da Rua Cornelius Frederick precisa de mais 2 sensores', '2021-04-10');
 
-INSERT INTO OCORRENCIAS VALUES(NULL, 2, 1, 'Sensores ativando e desativando o sistema de irrigação sem parar.', '2022-02-28','2022-02-28');
-INSERT INTO OCORRENCIAS VALUES(NULL, 1, 1, 'Sensores desligando sozinhos.', '2022-02-28','2022-02-28');
-INSERT INTO OCORRENCIAS VALUES(NULL, 1, 2, 'Sensores pegaram fogo', '2022-02-28','2022-02-28');
-INSERT INTO OCORRENCIAS VALUES(NULL, 2, 2, 'Sensores desligando sozinhos.', '2022-02-28','2022-02-28');
 SELECT * FROM OCORRENCIAS;
 
+------------------------------------------------------------
+-- JOIN ABAIXO MOSTRA A RELAÇÃO DE LOGIN'S COM OS CLIENTES
+------------------------------------------------------------
 
-select * from clientes;
-select * from funcionarios;
-SELECT * FROM LEADS;
+SELECT USUARIOS_CLIENTES.*, CLIENTES.NOME_FANTASIA FROM USUARIOS_CLIENTES JOIN CLIENTES 
+ON CLIENTES.ID_CLIENTE = FK_CLIENTES;
 
---                         A LINHA DE BAIXO MOSTRA
--- RELAÇÃO ENTRE OS FUNCIONARIOS QUE ATENDEM OS CLIENTES QUE FECHARAM CONTRATO COM A WISOY
---
+------------------------------------------------------------
+-- JOIN ABAIXO MOSTRA A RELAÇÃO DAS FAZENDAS COM SEUS CLIENTES
+------------------------------------------------------------
 
-select CLIENTES.*, FUNCIONARIOS.NOME FROM CLIENTES JOIN FUNCIONARIOS ON ID_FUNCIONARIO = FK_FUNCIONARIOS; 
+SELECT FAZENDAS.*, CLIENTES.NOME_FANTASIA FROM FAZENDAS JOIN CLIENTES ON CLIENTES.ID_CLIENTE = FK_CLIENTE;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
