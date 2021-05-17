3d78aa4a002da8727005c7c7ab05baf52e3c71f2
amandafruteirodelima <amanda.lima@bandtec.com.br>
2021-05-17 19:18:35 -0300

    atualização

--- NODE4_db/app/database.js
+++ NODE4_db/app/database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     port     : '3306',
-    user     : 'mvdd1998',
-    password : 'M370ez7@5524ma777',
+    user     : 'amanda_user',
+    password : 'chapeleiro',
     database : 'wisoy'
 });
 
