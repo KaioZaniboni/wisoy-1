d8b90e8e00199cb7de20d1fb7b559720cefc5821
fernandachimenez21 <fernanda.leme@bandtec.com.br>
2021-06-02 12:09:54 -0300

    Adicionando Manual de Instalação 02.06.2021

--- Site/api-projeto-site/public/funcoes.js
+++ Site/api-projeto-site/public/funcoes.js
@@ -8,6 +8,8 @@ function redirecionar_login() {
 function verificar_autenticacao() {
     login_usuario = sessionStorage.login_usuario_meuapp;
     nome_usuario = sessionStorage.nome_usuario_meuapp;
+    email_usuario = sessionStorage.nome_usuario_meuapp;
+    
     
     if (login_usuario == undefined)  {
         redirecionar_login();
