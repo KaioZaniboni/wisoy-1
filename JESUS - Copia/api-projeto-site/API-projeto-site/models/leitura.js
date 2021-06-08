9271a51c4d7b2c2650171e258ec2eb72bf5b4997
amandafruteirodelima <amanda.lima@bandtec.com.br>
2021-06-07 22:45:30 -0300

    atualização

---
+++ JESUS - Copia/api-projeto-site/API-projeto-site/models/leitura.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let Leitura = sequelize.define('Leitura',{	
+		id: {
+			field: 'ID_HISTORICO',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},	
+		fk_sensor: {
+			field: 'FK_SENSOR',
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
+		umidade: {
+			field: 'LEITURA_UMIDADE',
+			type: DataTypes.DECIMAL,
+			allowNull: false
+		},
+		leitura_data_hora: {
+			field: 'LEITURA_DATA_HORA',
+			type: DataTypes.DATE, // NÃO existe DATETIME. O tipo DATE aqui já tem data e hora
+			allowNull: false
+		},
+		momento_grafico: {
+			type: DataTypes.VIRTUAL, // campo 'falso' (não existe na tabela). Deverá ser preenchido 'manualmente' no select
+			allowNull: true
+		}
+	}, 
+	{
+		tableName: 'HISTORICO_SENSOR', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Leitura;
+};
